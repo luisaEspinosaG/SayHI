05c15718082760e46c886aed2dca4a65c72ffb11
luisaEspinosaG <marespinosa011@gmail.com>
2024-04-25 16:43:41 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hello react native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'BC7FCD',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
